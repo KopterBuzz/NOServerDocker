1c4a7646d0a88b8508b00b8e319fdeaddd562e1e
Gabor Nemeth <etgabo@gmail.com>
2025-11-03 23:23:34 +0000

    typo in git command again

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf ./rcon
 mkdir ./rcon
-git git clone https://github.com/Shockfront-Studios/Nuclear-Option-Server-Tools.git ./rcon
+git clone https://github.com/Shockfront-Studios/Nuclear-Option-Server-Tools.git ./rcon
 sudo docker build . -t noserver
