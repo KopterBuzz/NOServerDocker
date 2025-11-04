1c71f3086438d4b60fbfc92aebc365736dc76a0c
Gabor Nemeth <etgabo@gmail.com>
2025-11-04 10:47:19 +0000

    debug flask again

--- entrypoint.sh
+++ entrypoint.sh
@@ -133,11 +133,11 @@ DEF_RCON_PASSWORD="changeme"
 cd ./rcon/ServerControlPanel
 sed "s|$DEF_RCON_PORT|$RCON_PORT|g" config.py
 sed "s|$DEF_RCON_PASSWORD|$RCON_PASSWORD|g" config.py
-python3 app.py &
+python3 app.py
 
-cd ../../server
-echo "missions folder content: "
-echo $MISSIONS_DIR
-ls -l $MISSIONS_DIR
-chmod +x ./run_bepinex.sh
-./run_bepinex.sh -limitframerate 60 -ServerRemoteCommands 7779 -logFile server.log
+#cd ../../server
+#echo "missions folder content: "
+#echo $MISSIONS_DIR
+#ls -l $MISSIONS_DIR
+#chmod +x ./run_bepinex.sh
+#./run_bepinex.sh -limitframerate 60 -ServerRemoteCommands 7779 -logFile server.log
