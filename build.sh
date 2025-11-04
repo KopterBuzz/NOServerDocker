a24892e80c1c3f82604ca895e2c0dcba11b83475
Gabor Nemeth <etgabo@gmail.com>
2025-11-04 00:07:18 +0000

    comment out for debug

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -rf ./rcon
-mkdir ./rcon
-git clone https://github.com/Shockfront-Studios/Nuclear-Option-Server-Tools.git ./rcon
+#rm -rf ./rcon
+#mkdir ./rcon
+#git clone https://github.com/Shockfront-Studios/Nuclear-Option-Server-Tools.git ./rcon
 sudo docker build . -t noserver
