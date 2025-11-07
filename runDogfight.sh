c4d705b79730a8fe8b53be1dafedb2671c812c3d
Gabor Nemeth <etgabo@gmail.com>
2025-11-07 12:05:17 +0000

    fixing typo in dogfight config

--- runDogfight.sh
+++ runDogfight.sh
@@ -7,14 +7,14 @@ RCONPORT=50000
 for ((INDEX=1;INDEX<$QUOTA+1;INDEX++)); do
     sudo docker run -d \
         --cpuset-cpus=0,1 \
-        -p $SERVERPORT-$SERVERPORT:$QUERYPORT-$QUERYPORT/udp \
-        -p $SERVERPORT-$SERVERPORT:$QUERYPORT-$QUERYPORT/tcp \
+        -p $SERVERPORT-$QUERYPORT:$SERVERPORT-$QUERYPORT/udp \
+        -p $SERVERPORT-$QUERYPORT:$SERVERPORT-$QUERYPORT/tcp \
         -p $RCONPORT-$RCONPORT:$RCONPORT-$RCONPORT/tcp \
         -v "$(pwd)/dogfightMission":/missions \
         -v "$(pwd)/banlist":/banlist \
         -v "$(pwd)/replays":/replays noserver \
         --modded true \
-        --name "[EU]Broccoli Defrost Force Dogfight #$INDEX" \
+        --name "BROCCOLI DEFROST FORCE DOGFIGHT #"$INDEX"" \
         --password ""\
         --port-override true \
         --port-value $SERVERPORT \
@@ -23,12 +23,11 @@ for ((INDEX=1;INDEX<$QUOTA+1;INDEX++)); do
         --maxplayers 8 \
         --nostoptime 0 \
         --rconPort $RCONPORT \
-        --rconPassword "defaultpassword" \
+        --rconPassword "lofasz" \
         --fpsLimit 60 \
-        --rotationType 2
 
     ((SERVERPORT=$SERVERPORT + 2))
     ((QUERYPORT=$QUERYPORT + 2))
     ((RCONPORT++))
-done
+don
     
